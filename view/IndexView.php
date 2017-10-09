171afcf80e2b4d7e14b81f0acc7b175c654f5e34
Antonela Nespoli <antonela.nespoli@gmail.com>
2017-10-09 13:44:28 -0300

    #4 - Views

---
+++ view/IndexView.php
@@ -0,0 +1,27 @@
+<?php
+class IndexView extends View
+{
+  function mostrarIndex(){
+    $this->smarty->display('templates/index.tpl');
+  }
+
+  /*function mostrarCrearTareas(){
+    $this->assignarTareaForm();
+    $this->smarty->display('templates/formCrear.tpl');
+  }
+
+  function errorCrear($error, $titulo, $descripcion, $completada){
+    $this->assignarTareaForm($titulo, $descripcion, $completada);
+    $this->smarty->assign('error', $error);
+    $this->smarty->display('templates/formCrear.tpl');
+  }
+
+  private function assignarTareaForm($titulo='', $descripcion='', $completada=0){
+    $this->smarty->assign('titulo', $titulo);
+    $this->smarty->assign('descripcion', $descripcion);
+    $this->smarty->assign('completada', $completada);
+  }*/
+
+}
+
+ ?>
