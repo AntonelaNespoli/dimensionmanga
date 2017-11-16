12b52ef0047fa2f4bc721782456bc419b9425d4c
Antonela Nespoli <antonela.nespoli@gmail.com>
2017-11-15 23:26:35 -0300

    #32 - Story permisos completa

--- js/script.js
+++ js/script.js
@@ -10,13 +10,42 @@ $(document).ready(function () {
 function navigate(url) {
     $.get(url, function (data) {
         $('.main-content').html(data);
-        $('#cboxSuperUser').change(function () {
-            var id_user = $(".idUser").attr('id');
-            console.log(id_user);
+        $('.cboxSuperUser').change(function () {
+            var id_user = this.getAttribute('data-id');
             if (this.checked) {
-                navigate('http://localhost/dimensionmanga/permisoSuperUser/'+ id_user + '/1' );
+                $.ajax({
+                    url: 'http://localhost/dimensionmanga/permisoSuperUser/'+ id_user + '/1',
+                    type: "POST",
+                    success: function (res) {
+                        console.log(JSON.parse(res));
+                        res = JSON.parse(res);
+                        if (res.message) {
+                            $("#mensaje").html($('<div class="alert alert-success" role="alert"></div>').append(res.message));
+                        } else if (res.error) {
+                            $("#mensaje").html($('<div class="alert alert-danger" role="alert"></div>').append(res.error));
+                        }
+                    },
+                    error: function (err) {
+                        console.error(err);
+                    }
+                })
             }else{
-                navigate('http://localhost/dimensionmanga/permisoSuperUser/'+ id_user + '/0' );                
+                $.ajax({
+                    url: 'http://localhost/dimensionmanga/permisoSuperUser/'+ id_user + '/0',
+                    type: "POST",
+                    success: function (res) {
+                        console.log(JSON.parse(res));
+                        res = JSON.parse(res);
+                        if (res.message) {
+                            $("#mensaje").html($('<div class="alert alert-success" role="alert"></div>').append(res.message));
+                        } else if (res.error) {
+                            $("#mensaje").html($('<div class="alert alert-danger" role="alert"></div>').append(res.error));
+                        }
+                    },
+                    error: function (err) {
+                        console.error(err);
+                    }
+                })
             }
         });
     });
